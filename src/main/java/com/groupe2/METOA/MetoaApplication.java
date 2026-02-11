e5f7dfd0a6ac1a6f7bf843e8aeeae4bfeb16d0b6
kouna-samira <kounasamira3@gmail.com>
2026-02-11 23:36:32 +0100

    premiere sauvegarde

---
+++ src/main/java/com/groupe2/METOA/MetoaApplication.java
@@ -0,0 +1,13 @@
+package com.groupe2.METOA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetoaApplication.class, args);
+	}
+
+}
