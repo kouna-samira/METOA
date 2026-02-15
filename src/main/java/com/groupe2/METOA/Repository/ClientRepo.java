86e312341f03e2bb3186809473e62c3624ee0c5f
kouna-samira <kounasamira3@gmail.com>
2026-02-15 12:32:14 +0100

    Revert "Ajout DTO, Repository et Service Client"
    This reverts commit a4895aeef737795e47d1d522c22e92ac19cdd36a.

--- src/main/java/com/groupe2/METOA/Repository/ClientRepo.java
+++
@@ -1,7 +0,0 @@
-package com.groupe2.METOA.Repository;
-
-import com.groupe2.METOA.Entity.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepo extends JpaRepository<Client,String> {
-}
--- src/main/java/com/groupe2/METOA/Repository/TrajetRepo.java
+++
@@ -1,7 +0,0 @@
-package com.groupe2.METOA.Repository;
-
-import com.groupe2.METOA.Entity.Trajet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrajetRepo extends JpaRepository<Trajet,String> {
-}
