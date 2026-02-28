243828113085e37a3c80fe99d5d41ca35a931ca6
PradelBilong <pradelbilong1@gmail.com>
2026-03-01 00:46:09 +0100

    Package config ajouter avec sa classe!

---
+++ metoa/src/main/java/com/metoa/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.metoa.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "LOGONEDIGITAL TEKHUB ACADEMY",
+                        email = "contact@logonedigital.com",
+                        url = "https://logonedigital.com"
+                ),
+                title = "METOA",
+                description = "METOA",
+                termsOfService = "&copy; LOGONEDIGITAL",
+                version = "v1"
+        )
+)
+public class OpenApiConfig {
+}
