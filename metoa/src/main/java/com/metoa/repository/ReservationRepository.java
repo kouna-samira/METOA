e161404c441532eb772653b49c906f6d6889444c
PradelBilong <pradelbilong1@gmail.com>
2026-03-04 04:07:13 +0100

    petite modification dans le repository et service

--- metoa/src/main/java/com/metoa/repository/ReservationRepository.java
+++ metoa/src/main/java/com/metoa/repository/ReservationRepository.java
@@ -1,4 +1,4 @@
-package com.metoa.repository;
+/*package com.metoa.repository;
 
 import com.metoa.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,4 +16,26 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     // Vérifier si un passager a déjà réservé pour un trajet
     boolean existsByPassagerIdAndTrajetId(Long passagerId, Long trajetId);
+}*/
+package com.metoa.repository;
+
+import com.metoa.entity.Reservation;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    List<Reservation> findByPassagerId(Long passagerId);
+
+    List<Reservation> findByTrajetId(Long trajetId);
+
+    boolean existsByPassagerIdAndTrajetId(Long passagerId, Long trajetId);
+
+    @Override
+    @EntityGraph(attributePaths = {"trajet", "trajet.villeDepart", "trajet.villeArrivee", "passager"})
+    Optional<Reservation> findById(Long id);
 }
