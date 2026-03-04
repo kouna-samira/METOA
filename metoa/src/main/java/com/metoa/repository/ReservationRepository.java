0072a4b3afee999e987e0fa20df9ecf0b9ad229e
PradelBilong <pradelbilong1@gmail.com>
2026-03-04 12:21:18 +0100

    modification de ReservationRepository et ReservationServiceImpl

--- metoa/src/main/java/com/metoa/repository/ReservationRepository.java
+++ metoa/src/main/java/com/metoa/repository/ReservationRepository.java
@@ -1,22 +1,3 @@
-/*package com.metoa.repository;
-
-import com.metoa.entity.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.List;
-
-@Repository
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-
-    // Réservations d'un passager
-    List<Reservation> findByPassagerId(Long passagerId);
-
-    // Réservations pour un trajet
-    List<Reservation> findByTrajetId(Long trajetId);
-
-    // Vérifier si un passager a déjà réservé pour un trajet
-    boolean existsByPassagerIdAndTrajetId(Long passagerId, Long trajetId);
-}*/
 package com.metoa.repository;
 
 import com.metoa.entity.Reservation;
@@ -30,9 +11,7 @@ import java.util.Optional;
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     List<Reservation> findByPassagerId(Long passagerId);
-
     List<Reservation> findByTrajetId(Long trajetId);
-
     boolean existsByPassagerIdAndTrajetId(Long passagerId, Long trajetId);
 
     @Override
