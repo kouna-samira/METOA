8604ce0cb3b94365f3fef66d5499bef5c7e690ad
Dalfran <dalfrankamdem@gmail.com>
2026-03-11 09:46:58 +0100

    save profile Repo

--- src/main/java/com/groupe2/METOA/gestionProfilUtiisateur/repository/ProfilRepo.java
+++
@@ -1,7 +0,0 @@
-package com.groupe2.METOA.gestionProfilUtiisateur.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfilRepo extends JpaRepository<Profil, String>  {
-}
