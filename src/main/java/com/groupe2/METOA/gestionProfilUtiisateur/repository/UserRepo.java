dc466eaec1b9e1d993e8bc69ecbd7edd5bdc6e78
Dalfran <dalfrankamdem@gmail.com>
2026-03-11 11:41:30 +0100

    save user repo

--- src/main/java/com/groupe2/METOA/gestionProfilUtiisateur/repository/UserRepo.java
+++ src/main/java/com/groupe2/METOA/gestionProfilUtiisateur/repository/UserRepo.java
@@ -2,6 +2,18 @@ package com.groupe2.METOA.gestionProfilUtiisateur.repository;
 
 import com.groupe2.METOA.gestionProfilUtiisateur.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepo extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUserName(String userName);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUserName(String userName);
 }
