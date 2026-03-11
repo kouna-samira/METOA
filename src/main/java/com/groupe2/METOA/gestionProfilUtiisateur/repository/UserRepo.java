5ac9dad847c8d2c4f93ccd049773649aa026f9ad
Dalfran <dalfrankamdem@gmail.com>
2026-03-11 12:16:49 +0100

    save user new user repo

--- src/main/java/com/groupe2/METOA/gestionProfilUtiisateur/repository/UserRepo.java
+++ src/main/java/com/groupe2/METOA/gestionProfilUtiisateur/repository/UserRepo.java
@@ -3,7 +3,10 @@ package com.groupe2.METOA.gestionProfilUtiisateur.repository;
 import com.groupe2.METOA.gestionProfilUtiisateur.entity.Role;
 import com.groupe2.METOA.gestionProfilUtiisateur.entity.user.StatusUser;
 import com.groupe2.METOA.gestionProfilUtiisateur.entity.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,23 +15,20 @@ import java.util.Optional;
 @Repository
 public interface UserRepo extends JpaRepository<User, String> {
 
-    Optional<User> findByEmail(String email);
 
-    Optional<User> findByUserName(String userName);
-
-    boolean existsByEmail(String email);
-
-    boolean existsByUserName(String userName);
+    Optional<User> findByEmailIgnoreCase(String email);
 
     List<User> findByNomIgnoreCase(String nom);
 
     List<User> findByPrenomIgnoreCase(String prenom);
 
-    Optional<User> findByEmailIgnoreCase(String email);
-
-    List<User> findByVille(String ville);
-
-    List<User> findByRole(Role role);
-
-    List<User> findByStatusUser(StatusUser status);
+    @Query("""
+SELECT u FROM User u
+WHERE
+LOWER(u.nom) LIKE LOWER(CONCAT('%', :keyword, '%'))
+OR LOWER(u.prenom) LIKE LOWER(CONCAT('%', :keyword, '%'))
+OR LOWER(u.email) LIKE LOWER(CONCAT('%', :keyword, '%'))
+OR LOWER(u.ville) LIKE LOWER(CONCAT('%', :keyword, '%'))
+""")
+    Page<User> searchUsers(String keyword, Pageable pageable);
 }
