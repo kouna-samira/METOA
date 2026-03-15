a441154e74773a36ad9b88c09c0af7a2ea4e446a
PradelBilong <pradelbilong1@gmail.com>
2026-03-15 21:38:18 +0100

    copies du package exception  dans le package com.groupe2.METOA.exception fini

---
+++ metoa/src/main/java/com/groupe2/METOA/exception/ResourceExistsException.java
@@ -0,0 +1,7 @@
+package com.groupe2.METOA.exception;
+
+public class ResourceExistsException extends RuntimeException {
+    public ResourceExistsException(String message) {
+        super(message);
+    }
+}
