cdf71f25d226f07c78ec0e658b24eb3cbbb3bba8
kouna-samira <kounasamira3@gmail.com>
2026-07-04 10:49:01 +0100

    connection avec front

--- src/main/java/com/groupe2/METOA/Controller/ReservationController.java
+++ src/main/java/com/groupe2/METOA/Controller/ReservationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
